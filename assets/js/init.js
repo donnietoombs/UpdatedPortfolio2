8deb09afbe29144f072577610e25d3bf15042bd4
Donnie Toombs <donnietoombs@gmail.com>
2019-01-25 17:26:22 -0500

    update with video

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,11 +1,1 @@
-(function($){
-  $(function(){
-
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
-    $('.modal').modal();
-    $('.carousel').carousel();
-    $('.modal-trigger').leanModal();
-    
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+M.AutoInit();
