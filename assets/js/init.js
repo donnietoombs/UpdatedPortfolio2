697dd868d87c1b159bfe595b2f97c33410a885b4
Donnie Toombs <donnietoombs@gmail.com>
2019-01-25 02:29:39 -0500

    updates to portfolio site

--- assets/js/init.js
+++ assets/js/init.js
@@ -5,7 +5,7 @@
     $('.parallax').parallax();
     $('.modal').modal();
     $('.carousel').carousel();
-      
+    $('.modal-trigger').leanModal();
     
   }); // end of document ready
 })(jQuery); // end of jQuery name space
